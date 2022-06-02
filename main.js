54bd218273252eb5c154b54afcb181e79487baf6
lWolgrand <lopeswolgrand@gmail.com>
2022-06-02 19:34:27 -0300

    Refatorando função para allSong onde se espera um parâmetro idElementoAudio

--- main.js
+++ main.js
@@ -1,56 +1,8 @@
-function somPom() {
+function allSong(idelementoAudio) {
 
-  document.querySelector('#som_tecla_pom').play();
+  document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = somPom;
-
-function somClap()) {
-
-  document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = somClap;
-
-function somTim() {
-
-  document.querySelector('#som_tecla_tim').play();
-}
-document.querySelector('.tecla_tim').onclick = somTim;
-
-function somPuff() {
-
-  document.querySelector('#som_tecla_puff').play();
-}
-document.querySelector('.tecla_puff').onclick = somPuff;
-
-function somSplash() {
-
-  document.querySelector('#som_tecla_splash').play();
-}
-document.querySelector('.tecla_splash').onclick = somSplash;
-
-function somToim() {
-
-  document.querySelector('#som_tecla_toim').play();
-}
-document.querySelector('.tecla_toim').onclick = somToim;
-
-function somPsh() {
-
-  document.querySelector('#som_tecla_psh').play();
-}
-document.querySelector('.tecla_psh').onclick = somPsh;
-
-function somTic() {
-
-  document.querySelector('#som_tecla_tic').play();
-}
-document.querySelector('.tecla_tic').onclick = somTic;
-
-function somTom() {
-
-  document.querySelector('#som_tecla_tom').play();
-}
-document.querySelector('.tecla_tom').onclick = somTom;
+const teclaList = document.querySelectorAll('.tecla');
 
 
 
