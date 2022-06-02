a89ce0d5137082718368ddb6439a7bab0dd334ac
lWolgrand <lopeswolgrand@gmail.com>
2022-06-02 19:04:00 -0300

    Criando função somClap

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ function somPom() {
 }
 document.querySelector('.tecla_pom').onclick = somPom;
 
+function somClap()) {
 
+  document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = somClap;
 
 
 
