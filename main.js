33808be87d52c698afe8fd144eb37068cbff33d9
lWolgrand <lopeswolgrand@gmail.com>
2022-06-02 22:07:31 -0300

    Criando melhor lógica para funções de cada botão.

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ const teclaList = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
+while (contador < teclaList.length) {
+
+  const tecla = teclaList[contador];
+  const instrumento = tecla.classList[1];
+
+  // const idAudio = `#som_${instrumento}`;
+
+  console.log(instrumento);
+
+  // tecla.onclick = function () {
+  //   allSong('');
+  // }
+  contador++;
+  console.log(contador);
+}
+
 
 
 
